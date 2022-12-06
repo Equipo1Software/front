253cbbcb440302178497279b02bfea1e4fc9539c
Nicolas Silva <njsilvapereira@gmail.com>
2022-12-06 00:38:50 -0300

    login

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,12 +5,12 @@ import { Heading,Flex, Input,Button} from '@chakra-ui/react'
 export default function Home() {
   return (
     <Flex height={"100vh"} align={"center"} justify={"center"}>
-      <Flex direction={"column"} backgroundColor={"#BAD1C2"} p={12}>
-        <Heading color={"#153462"} size="2xl">Log In</Heading>
+      <Flex direction={"column"} backgroundColor={"#BAD1C2"} p={12} width={"400px"} height={"460px"}>
+        <Heading color={"#153462"} size="2xl" my={1}>Log In</Heading>
         <Heading size={"sm"} my={4} color={"#153462"}>Grupo 5 - Gastos Comunes</Heading>
-        <Input my={3} placeholder="Email" type={"text"}/>
-        <Input my={2} placeholder="*******" type={"password"}/>
-        <Button my={4} backgroundColor={"#153462"} color={"#F6F6C9"} >Ingresar</Button>
+        <Input my={4} placeholder="Email" type={"text"} width={"308x"}/>
+        <Input my={2} placeholder="*******" type={"password"} width={"308px"}/>
+        <Button my={5} backgroundColor={"#153462"} color={"#F6F6C9"} >Ingresar</Button>
       </Flex>
     </Flex>
   )
