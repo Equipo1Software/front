592db996cd0783bf57aa2c481dc7d44a4edbadc7
Cristobal Roman <romanleiva1998@gmail.com>
2022-12-29 20:31:53 -0300

    creacion portal de pago

---
+++ pages/user/payment.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../components/Navbar';
+import Sidebar from '../components/Sidebar';
+
+const payment = ()=>{
+    return(
+        <div className="payment">
+            <Navbar/>
+            
+        </div>
+    );
+}
+
+export default payment;
